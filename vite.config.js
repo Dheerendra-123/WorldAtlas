d4acb4b34f443303aad21eebb9a04ed7a94ab0e6
Dheerendra Vikram Dixit <dheerendradixit321@gmail.com>
2025-04-18 10:00:32 +0530

    jj

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base:"/WorldAtlas",
-=======
->>>>>>> e0f9d35 (hi)
   
 })
